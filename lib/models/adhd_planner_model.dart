6c77c94f7fe084facf2a5bdf1e7f9c0e299b7dcb
JorisGreywolf <joris.barbe.pro@gmail.com>
2023-09-07 14:25:28 +0200

    i have no clue what i did

--- lib/models/adhd_planner_model.dart
+++ lib/models/adhd_planner_model.dart
@@ -24,8 +24,40 @@ class ADHDPlannerModel extends HiveObject {
   ADHDPlannerModel(this.taskText, this.id, this.isChecked, this.creationDate,
       this.deadlineDate, this.taskType, this.taskStatus);
 
-  factory ADHDPlannerModel.fromJson(Map<String, dynamic> json) =>
-      _$ADHDPlannerModelFromJson(json);
+  factory ADHDPlannerModel.fromJson(Map<String, dynamic> json) {
+    return ADHDPlannerModel(
+      json['taskText'] as String? ?? '',
+      json['id'] as String? ?? '',
+      json['isChecked'] as bool? ?? false,
+      _parseCustomDate(json['creationDate']),
+      _parseCustomDate(json['deadlineDate']),
+      json['taskType'] as String? ?? '',
+      json['taskStatus'] as String? ?? '',
+    );
+  }
+
+  static DateTime _parseCustomDate(String? dateStr) {
+    if (dateStr == null || dateStr.isEmpty) {
+      // Return a default date value, such as the current date/time, or any other appropriate default.
+      return DateTime.now();
+    }
+
+    try {
+      // Implement your custom date parsing logic here.
+      // Example: Parse dateStr into DateTime using a specific format.
+      // DateTime parsedDate = customDateParsingLogic(dateStr);
+      // return parsedDate;
+
+      // For simplicity, let's assume dateStr is in ISO 8601 format.
+      return DateTime.parse(dateStr);
+    } catch (e) {
+      // If parsing fails, return a default date value or handle the error as needed.
+      // You might want to log the error or throw a custom exception.
+      return DateTime.now();
+    }
+  }
+
+
 
   Map<String, dynamic> toJson() => _$ADHDPlannerModelToJson(this);
 }
